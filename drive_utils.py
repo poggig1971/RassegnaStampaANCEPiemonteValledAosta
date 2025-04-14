18df2b87ad8be91ac6d42be7a053a09e776d8864
poggig1971 <poggig@yahoo.com>
2025-04-14 15:36:12 +0200

    Update drive_utils.py

--- drive_utils.py
+++ drive_utils.py
@@ -21,7 +21,8 @@ def authenticate():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file('.streamlit/client_secret.json', SCOPES)
-            creds = flow.run_console()
+            creds = flow.run_local_server(port=0)
+
         with open(TOKEN_PATH, 'wb') as token:
             pickle.dump(creds, token)
     return creds
