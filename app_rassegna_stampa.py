f68dc4659a6e9a5773e1156d5b2d2fb804d634a0
poggig1971 <poggig@yahoo.com>
2025-04-16 13:26:40 +0200

    Update app_rassegna_stampa.py

--- app_rassegna_stampa.py
+++ app_rassegna_stampa.py
@@ -19,7 +19,7 @@ from drive_utils import (
 # === CONFIGURAZIONE ===
 col1, col2 = st.columns([1, 5])
 with col1:
-    st.image("logo.png", width=100)
+    st.image("logo.png", width=200)
 with col2:
     st.markdown("### **ANCE Piemonte Valle d'Aosta**")
     st.markdown("#### _Rassegna Stampa Digitale_")
