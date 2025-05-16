d8e30f88f03c30a3148c941d6d947d2456395e42
poggig1971 <poggig@yahoo.com>
2025-05-16 18:26:47 +0200

    Update app_rassegna_stampa.py

--- app_rassegna_stampa.py
+++ app_rassegna_stampa.py
@@ -144,11 +144,11 @@ def main():
                         st.info("🔍 Nessun utente registrato.")
             
             st.markdown("### 🔁 Carica nuovo file utenti.csv")
-            uploaded = st.file_uploader("Scegli file utenti.csv", type="csv")
-            if uploaded:
-            upload_pdf_to_drive(service, uploaded, "utenti.csv", is_memory_file=True, overwrite=True)
-            st.success("✅ utenti.csv aggiornato.")
-            st.rerun()
+uploaded = st.file_uploader("Scegli file utenti.csv", type="csv")
+if uploaded:
+    upload_pdf_to_drive(service, uploaded, "utenti.csv", is_memory_file=True, overwrite=True)
+    st.success("✅ utenti.csv aggiornato.")
+    st.rerun()
             
             
             else:
