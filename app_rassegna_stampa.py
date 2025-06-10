2409de6c761ad302efee1af9fc1ad3221327ccae
poggig1971 <poggig@yahoo.com>
2025-06-10 19:20:21 +0200

    Update app_rassegna_stampa.py

--- app_rassegna_stampa.py
+++ app_rassegna_stampa.py
@@ -187,7 +187,7 @@ def mostra_statistiche_user():
 
         df['fascia'] = df['ora'].apply(fascia_oraria)
         heatmap_data = df.groupby(['giorno_settimana', 'fascia']).size().unstack(fill_value=0)
-        giorni_ordine = ['Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab', 'Dom']
+        giorni_ordine = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
         fasce_ordine = ['Notte', 'Mattino', 'Pomeriggio', 'Sera']
         heatmap_data = heatmap_data.reindex(giorni_ordine).reindex(columns=fasce_ordine)
 
